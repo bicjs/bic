42d0f095eaca98e6b163d964fd82ba25d5dfb677
Ray Patterson <ray_patterson@apple.com>
2017-04-04 17:56:13 -0700

    fix(modernizr): giving up

--- packages/plugins/bic-plugin-common-chunks/src/index.js
+++ packages/plugins/bic-plugin-common-chunks/src/index.js
@@ -6,13 +6,22 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackChunkHash = require('webpack-chunk-hash');
 const ChunkManifestPlugin = require('chunk-manifest-webpack-plugin');
-const InlineChunkManifestHtmlWebpackPlugin = require('inline-chunk-manifest-html-webpack-plugin');
+const InlineChunkManifestPlugin = require('inline-chunk-manifest-html-webpack-plugin');
 
 const log = require('@bicjs/bic-logger').get('plugin', 'common chunks');
 const cfg = require('@bicjs/bic-config').get();
 
 /**
- * TODO: Disable `common` JS if `webpackConfig.entry.length === 1`.
+ *
+ * NOTE: We're not chunking and inlining Modernizr because:
+ *
+ * 		1. Paul Irish says it's [irresponsible](https://github.com/Modernizr/Modernizr/issues/878#issuecomment-41448059)
+ *   	2. The `HtmlWebpackPlugin` won't order the Modernizr chunk as a dependency.
+ *    3. The `ScriptExtHtmlWebpackPlugin` can't inline scripts into the `<head>` tag.
+ *    4. Allowing this plugin to have knoledge of Modernizr is breaking encapsulation
+ *    5. Feature detection & progressive enhancement vs. polyfilling is open to debate.
+ *    6. I'm tired.
+ *
  */
 
 module.exports = webpackConfig => {
@@ -23,48 +32,44 @@ module.exports = webpackConfig => {
 
 	const minChunks = chunks.length;
 
+	const isExternal = module => {
+
+		return module.context && module.context.indexOf(cfg.file.node) >= 0;
+
+	};
+
+	const isCommon = count => {
+
+		return count >= minChunks;
+
+	};
+
 	if (cfg.production === true) {
-
-		/**
-		 * Chunk "vendor" code loaded from `node_modules`.
-		 */
-		webpackConfig.plugins.push(new webpack.optimize.CommonsChunkPlugin({
-			chunks,
-			name: cfg.file.vendor,
-			minChunks: (module, count) => {
-
-				console.log(cfg.file.vendor, module.resource);
-
-				return module.context && module.context.indexOf(cfg.file.node) >= 0 && count >= minChunks;
-
-			}
-		}));
-
-		chunks.push(cfg.file.vendor);
-
-		/**
-		 * Chunk "modernizr" code.
-		 * TODO: Inline Modernizr JS into head?
-		 */
-		webpackConfig.plugins.push(new webpack.optimize.CommonsChunkPlugin({
-			chunks,
-			name: cfg.file.modernizr,
-			minChunks: (module, count) => {
-
-				console.log(cfg.file.modernizr, module.resource);
-
-				return module.resource && new RegExp(cfg.file.modernizr).test(module.resource) && count >= minChunks;
-
-			}
-		}));
 
 		/**
 		 * Chunk "common" code loaded in all entry points.
 		 */
 		webpackConfig.plugins.push(new webpack.optimize.CommonsChunkPlugin({
-			chunks,
 			name: cfg.file.common,
-			minChunks
+			chunks,
+			minChunks: (module, count) => {
+
+				return !isExternal(module) && isCommon(count);
+
+			}
+		}));
+
+		/**
+		 * Chunk "vendor" code loaded from `node_modules`.
+		 */
+		webpackConfig.plugins.push(new webpack.optimize.CommonsChunkPlugin({
+			chunks: chunks.concat(cfg.file.common),
+			name: cfg.file.vendor,
+			minChunks: (module, count) => {
+
+				return isExternal(module) && isCommon(count);
+
+			}
 		}));
 
 		/**
@@ -78,7 +83,7 @@ module.exports = webpackConfig => {
 		webpackConfig.plugins.push(new webpack.HashedModuleIdsPlugin());
 		webpackConfig.plugins.push(new WebpackChunkHash());
 		webpackConfig.plugins.push(new ChunkManifestPlugin());
-		webpackConfig.plugins.push(new InlineChunkManifestHtmlWebpackPlugin());
+		webpackConfig.plugins.push(new InlineChunkManifestPlugin());
 
 	}
 
@@ -104,12 +109,7 @@ module.exports = webpackConfig => {
 			filename,
 			inject,
 			minify,
-			chunks,
-			chunksSortMode: (moduleA, moduleB) => {
-
-				return moduleB.id > moduleA.id;
-
-			}
+			chunks
 		}));
 
 		return arr;
