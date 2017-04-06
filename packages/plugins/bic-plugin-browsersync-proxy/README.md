f31e83771922984b9652fb566394048f789df0da
Ray Patterson <ray_patterson@apple.com>
2017-04-05 17:03:48 -0700

    feat(browsersync): added browsersync proxy

---
+++ packages/plugins/bic-plugin-browsersync-proxy/README.md
@@ -0,0 +1,3 @@
+# Bic Plugin - browsersync proxy
+
+> Bic website packaging framework browsersync proxy plugin.
---
+++ packages/plugins/bic-plugin-browsersync-proxy/src/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+const log = require('@bicjs/bic-logger').get('plugin', 'browsersync proxy');
+const cfg = require('@bicjs/bic-config').get();
+
+module.exports = webpackConfig => {
+
+	if (cfg.production === false) {
+
+		webpackConfig.plugins.push(new BrowserSyncPlugin({
+			host: cfg.server.host,
+			port: cfg.server.port + 1,
+			proxy: cfg.server.url
+		}, {
+			reload: false
+		}));
+
+		log.debug('added');
+
+	}
+
+};
